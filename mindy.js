4c0d0ad50bbdcff6f07df25e9470d64ec089eef1
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2016-12-13 21:46:26 +0000

    Adding a few more commands.

--- mindy.js
+++ mindy.js
@@ -13,7 +13,7 @@ minecraft.on('chat', function(packet) {
         var msg = jsonMsg.with[1];
         if (username === minecraft.username) 
             return;
-        twitch.action(twitch.opts.channels[0], username + ': ' + msg);
+        twitch.action(twitch.opts.channels[0], '[' + username + '] ' + msg);
     }
 });
 minecraft.on('login', function(first, second) {
@@ -36,16 +36,25 @@ twitch.on('chat', function(channel, user, message, self) {
         minecraft.write('chat', { message: '/weather rain 30' });
     } else if (message == 'clear') {
         minecraft.write('chat', { message: '/weather clear' });
+    } else if (message == 'slime') {
+        minecraft.write('chat', { message: '/execute @a ~1 ~3 ~ /summon minecraft:slime ~1 ~3 ~ {Size:0,CustomName:"Twitch Slime",CustomNameVisible:1,Glowing:1,Health:20,Attributes:[{Name:"generic.movementSpeed",Base:1f},{Name:"generic.attackDamage",Base:0}],HandDropChances:[2F,2F],HandItems:[{id:"minecraft:apple",tag:{display:{Name:"Twitch Apple"}},Count:1},{}]}' });
+        twitch.action(twitch.opts.channels[0], "Summoning slimes around each player.");
+        minecraft.write('chat', { message: user.username + " has summoned slimes!" });
+    } else if (message == 'lighting') {
+        twitch.action(twitch.opts.channels[0], "Your anger will be displayed!");
+        minecraft.write('chat', { message: '/execute @a ~ ~ ~ /summon LightingBolt' });
+        minecraft.write('chat', { message: "You have been cursed by the god " + user.username + "!!!" });
     } else if (message == 'help') {
         twitch.action(twitch.opts.channels[0], "'rain' - make weather rainy.");
         twitch.action(twitch.opts.channels[0], "'clear' - make weather clear.");
+        twitch.action(twitch.opts.channels[0], "'slime' - summon attack slimes.");
+        twitch.action(twitch.opts.channels[0], "'lightning' - strike players with lighting.");
     } else {
-        minecraft.write('chat', { message: user.username + ': ' + message });
+        minecraft.write('chat', { message: '[' + user.username + '] ' + message });
     }
 });
 twitch.on("join", function (channel, username, self) {
     minecraft.write('chat', { message: username + ' has joined twitch' });
-    twitch.action(twitch.opts.channels[0], "Hello " + username + ". I'm a bot! Type 'help' to interact with minecraft.");
 });
 twitch.on("part", function (channel, username, self) {
     minecraft.write('chat', { message: username + ' has left twitch' });
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-// http://stackoverflow.com/questions/4797050/how-to-run-process-as-background-and-never-die
+# http://stackoverflow.com/questions/4797050/how-to-run-process-as-background-and-never-die
 
-// Detach from TTY and run as a daemon.
+# Detach from TTY and run as a daemon.
 screen -dmS mindy node mindy.js
 
-// To reattach: screen -r mindy
+# To reattach: screen -r mindy
